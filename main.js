49263fdb22e8a628377e52f52c5b0472a3983ff5
Javier Razo <javier.razo1221@gmail.com>
2022-11-16 12:44:43 -0700

    fixed margins

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     "use strict"
 
     function renderCoffee(coffee) {
@@ -21,34 +21,35 @@
     }
 
 
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    let selectedRoast = roastSelection.value;
-    let filteredCoffees = [];
-    coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        } else if (roastSelection.value === 'all') {
-            filteredCoffees.push(coffee);
-        }
-    });
+    function updateCoffees(e) {
+        e.preventDefault(); // don't submit the form, we just want to update the data
+        let selectedRoast = roastSelection.value;
+        let filteredCoffees = [];
+        coffees.forEach(function (coffee) {
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
+            } else if (roastSelection.value === 'all') {
+                filteredCoffees.push(coffee);
+            }
+        });
         tbody.innerHTML = renderCoffees(filteredCoffees);
 
-}
+    }
+
     function searchCoffees() {
         let searchRoast = searchBox.value.toUpperCase();
         let filteredCoffees = [];
-        coffees.forEach(function(coffee) {
+        coffees.forEach(function (coffee) {
             if (coffee.name.toUpperCase().includes(searchRoast)) {
                 filteredCoffees.push(coffee);
             }
         });
         tbody.innerHTML = renderCoffees(filteredCoffees);
     }
-    function addCoffee (event) {
+
+    function addCoffee(event) {
         event.preventDefault();
-        var addID = coffees.length+1;
+        var addID = coffees.length + 1;
         var addName = addCoffeeName.value.toString();
         var addRoast = addCoffeeRoast.value.toString();
         let input = {id: addID, name: addName, roast: addRoast};
@@ -60,22 +61,22 @@ function updateCoffees(e) {
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-        var coffees = [
-            {id: 1, name: 'Light City', roast: 'light'},
-            {id: 2, name: 'Half City', roast: 'light'},
-            {id: 3, name: 'Cinnamon', roast: 'light'},
-            {id: 4, name: 'City', roast: 'medium'},
-            {id: 5, name: 'American', roast: 'medium'},
-            {id: 6, name: 'Breakfast', roast: 'medium'},
-            {id: 7, name: 'High', roast: 'dark'},
-            {id: 8, name: 'Continental', roast: 'dark'},
-            {id: 9, name: 'New Orleans', roast: 'dark'},
-            {id: 10, name: 'European', roast: 'dark'},
-            {id: 11, name: 'Espresso', roast: 'dark'},
-            {id: 12, name: 'Viennese', roast: 'dark'},
-            {id: 13, name: 'Italian', roast: 'dark'},
-            {id: 14, name: 'French', roast: 'dark'},
-        ];
+    var coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
 
 
     var tbody = document.querySelector('#coffees');
@@ -92,8 +93,8 @@ function updateCoffees(e) {
     addCoffeeButton.addEventListener('click', addCoffee);
 
     var cookieValue = document.getElementById("coffees");
-    var value = cookieValue .getAttribute('add-coffee-roast');
-    if(typeof(Storage)!=="undefined") {
+    var value = cookieValue.getAttribute('add-coffee-roast');
+    if (typeof (Storage) !== "undefined") {
         //     alert(value);
         localStorage["GetData"] = value;
         //     alert(localStorage["GetData"]);
